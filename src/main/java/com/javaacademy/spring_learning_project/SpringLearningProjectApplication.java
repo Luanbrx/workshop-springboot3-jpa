a64ef0f920464f80e0442f1e57ff1b1b5471da58
luan menezesds <luanmenezesds615@gmail.com>
2025-09-18 17:51:31 -0300

    Project created

---
+++ src/main/java/com/javaacademy/spring_learning_project/SpringLearningProjectApplication.java
@@ -0,0 +1,13 @@
+package com.javaacademy.spring_learning_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLearningProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearningProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/javaacademy/spring_learning_project/SpringLearningProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javaacademy.spring_learning_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringLearningProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
