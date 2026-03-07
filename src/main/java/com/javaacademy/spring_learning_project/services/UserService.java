db127db759e11e3564d76ca650bb153529beace8
luan menezesds <luanmenezesds615@gmail.com>
2026-03-06 23:56:36 -0300

    Exception handling - update

--- src/main/java/com/javaacademy/spring_learning_project/services/UserService.java
+++ src/main/java/com/javaacademy/spring_learning_project/services/UserService.java
@@ -4,6 +4,7 @@ import com.javaacademy.spring_learning_project.entities.User;
 import com.javaacademy.spring_learning_project.exceptions.DatabaseException;
 import com.javaacademy.spring_learning_project.exceptions.ResourceNotFoundException;
 import com.javaacademy.spring_learning_project.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
@@ -44,9 +45,14 @@ public class UserService {
     }
 
     public User update(Long id, User obj) {
-        User entity = repository.getReferenceById(id);
-        updateData(entity, obj);
-        return repository.save(entity);
+       try {
+           User entity = repository.getReferenceById(id);
+           updateData(entity, obj);
+           return repository.save(entity);
+       } catch (EntityNotFoundException e){
+           throw new ResourceNotFoundException(id);
+       }
+
     }
     private void updateData(User entity, User obj){
         entity.setName(obj.getName());
